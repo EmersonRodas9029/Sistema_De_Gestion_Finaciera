f33c1dcdd46c93bc1f60a219997437b9bcfdc60b
theri <theripper446@gmail.com>
2025-09-10 21:47:00 -0600

    Configuración de las migraciones necesarias para el sistema

---
+++ src/main/resources/db/migration/V13__create_indices.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_gastos_cliente_fecha ON gastos(cliente_id, fecha);
+CREATE INDEX idx_gastos_categoria ON gastos(categoria_id);
+CREATE INDEX idx_ingresos_cliente_fecha ON ingresos(cliente_id, fecha);
+CREATE INDEX idx_notificaciones_cliente_fecha ON notificaciones(cliente_id, fecha_creacion);
+CREATE INDEX idx_usuarios_email ON usuarios(email);
+CREATE INDEX idx_clientes_documento ON clientes(documento_identidad);
